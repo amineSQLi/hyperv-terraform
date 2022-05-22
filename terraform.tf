36e88e0f6cded57a3e1ba9fe23c5826894f0f863
amineSQLi <amineelmeskaoui@gmail.com>
2022-05-22 19:17:25 +0100

    adding ressource section

--- terraform.tf
+++ terraform.tf
@@ -26,3 +26,44 @@ resource "hyperv_vhd" "webserver" {
 # Create a machine
 resource "hyperv_machine_instance" "webserver" {
 }
+
+resource "hyperv_machine_instance" "default" {
+  name = "WebServer"
+  generation = 1
+  automatic_critical_error_action = "Pause"
+  automatic_critical_error_action_timeout = 30
+  automatic_start_action = "StartIfRunning"
+  automatic_start_delay = 0
+  automatic_stop_action = "Save"
+  checkpoint_type = "Production"
+  dynamic_memory = false
+  guest_controlled_cache_types = false
+  high_memory_mapped_io_space = 536870912
+  lock_on_disconnect = "Off"
+  low_memory_mapped_io_space = 134217728
+  memory_maximum_bytes = 1099511627776
+  memory_minimum_bytes = 536870912
+  memory_startup_bytes = 536870912
+  notes = ""
+  processor_count = 1
+  smart_paging_file_path = "C:\ProgramData\Microsoft\Windows\Hyper-V"
+  snapshot_file_location = "C:\ProgramData\Microsoft\Windows\Hyper-V"
+  static_memory = true
+  state = "Running"
+
+  # Configure integration services
+  integration_services {
+  }
+
+  # Create a network adaptor
+  network_adaptors {
+  }
+
+  # Create dvd drive
+  dvd_drives {
+  }
+
+  # Create a hard disk drive
+  hard_disk_drives {
+  }
+}
